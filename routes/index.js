8d0afde9dd27bc6bf6f749e02f60cea3eb693f11
joaquin8123 <joaquin8123@gmail.com>
2021-03-05 21:25:37 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const api = express.Router()
 const proyectController = require('../controllers/proyectosController')
+const tareasController = require('../controllers/tareasController')
 const {body} = require('express-validator/check')
+//PROYECTOS
 //GETS
 api.get('/',proyectController.home)
 api.get('/nosotros',proyectController.nosotros)
@@ -10,6 +12,16 @@ api.get('/proyectos/:url',proyectController.proyectoPorUrl)
 api.get('/proyecto/editar/:id',proyectController.formularioEditar)
 //POSTS
 api.post('/nuevo-proyecto/:id?',body('nombre').not().isEmpty().trim().escape() ,proyectController.nuevoProyecto)
-//api.post('/nuevo-proyecto/:id',body('nombre').not().isEmpty().trim().escape() ,proyectController.actualizarProyecto)
-
+//DELETS
+api.delete('/proyectos/:url',proyectController.eliminarProyecto)
+// FIN PROYECTOS
+// TAREAS
+//GETS
+//POSTS
+api.post('/proyectos/:url?',body('tarea').not().isEmpty().trim().escape() ,tareasController.nuevaTarea)
+//DELETES 
+api.delete('/proyectos/eliminar-tarea/:id',tareasController.eliminarTarea)
+//UPDATES
+api.put('/proyectos/:id',tareasController.actualizarTarea)
+// FIN TAREAS
 module.exports = api 
