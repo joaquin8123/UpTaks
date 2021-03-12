db19cda2054d79320738e3334897afa85e1e5b8e
joaquin8123 <joaquin8123@gmail.com>
2021-03-12 00:07:59 -0300

    Create Usuario.js

---
+++ models/Usuario.js
@@ -0,0 +1,50 @@
+const Sequelize = require('sequelize')
+const db = require('../database')
+const Proyectos = require('./Proyectos')
+const bcrypt = require('bcrypt-nodejs')
+
+const Usuario = db.define('usuarios',{
+    id : {
+        type : Sequelize.INTEGER(11),
+        primaryKey : true,
+        autoIncrement : true
+    },
+    email: {
+        type : Sequelize.STRING(60),
+        allowNull : false,
+        validate :{
+            isEmail: {
+                msg: 'Agrega un email valido'
+            },
+            notEmpty : {
+                msg: 'Agrega un email valido'
+            }
+        },
+        unique : {
+            args: true,
+            msg: 'El usuario ya existe!'
+        }
+    },
+    password: {
+        type : Sequelize.STRING(60),
+        allowNull : false,
+        validate :{
+            notEmpty : {
+                msg: 'Agrega un password valido'
+            }
+         }
+    }
+
+}, {
+    hooks: {
+        beforeCreate(usuario){
+            usuario.password = bcrypt.hashSync(usuario.password, bcrypt.genSaltSync(10))
+        }
+    }
+})
+Usuario.prototype.verificarPassword = function(password){
+    return bcrypt.compareSync(password,this.password)
+}
+//Usuario.hasMany(Proyectos)
+
+module.exports = Usuario
