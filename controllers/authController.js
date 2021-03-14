dd355f758df08d6b576d46972af9e45fc002587f
joaquin8123 <joaquin8123@gmail.com>
2021-03-14 15:37:57 -0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,10 @@
 const passport = require('passport')
+const Usuario = require('../models/Usuario')
+const crypto = require('crypto')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
+const bcrypt = require('bcrypt-nodejs')
+const enviarEmail = require('../handlers/email')
 
 exports.autenticarUsuario = passport.authenticate('local',{
     successRedirect: '/',
@@ -15,3 +21,53 @@ exports.cerrarSesion = (req,res,next) => {
        res.redirect('/iniciar-sesion')
    })
 }
+exports.enviarToken = async (req,res) => {
+    const usuario = await Usuario.findOne({where: {email: req.body.email}})
+    if(!usuario){
+        res.render('reestablecer', {
+            PageName: 'Reestablecer Contraseña',
+            error: 'El usuario no existe!'
+        });
+    }
+    usuario.token = crypto.randomBytes(20).toString('hex')
+    usuario.expiracion = Date.now() + 3600000
+    await usuario.save()
+    const resetUrl = `http://${req.headers.host}/reestablecer/${usuario.token}`
+    await enviarEmail.enviar({
+        usuario,
+        subject: 'Password Reset',
+        resetUrl,
+        archivo : 'reestablecer-password'
+    })
+    res.redirect('/iniciar-sesion')
+ }
+
+ exports.validarToken = async (req,res) => {
+    const usuario = Usuario.findOne({where: {token: req.params.token}})
+    if(!usuario){
+        error = 'No valido'
+        res.redirect('/reestablecer')
+    }
+    res.render('resetPassword',{
+        PageName : 'Reestablecer Contraseña'
+    }) 
+ }
+ exports.actualizarPassword = async (req,res) => {
+    const usuario = await Usuario.findOne({
+        where: {
+            token: req.params.token,
+            expiracion: {
+                [Op.gte] : Date.now()
+            }
+        }})
+    if(!usuario){
+        error = 'No valido'
+        res.redirect('/reestablecer')
+    }
+    usuario.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+    usuario.token = null
+    usuario.expiracion = null
+    await usuario.save()
+    res.redirect('/iniciar-sesion')
+ }
+ 
