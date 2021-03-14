1647ca41ed7c1ef1b208ab6d9ed09d7f2dd1bc45
joaquin8123 <joaquin8123@gmail.com>
2021-03-14 15:38:06 -0300

    Update usuarioController.js

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,5 @@
 const Usuario = require('../models/Usuario')
+const enviarEmail = require('../handlers/email')
 const usuarioController = {}
 
 usuarioController.formCuenta = (req, res,next) =>{
@@ -10,6 +11,19 @@ usuarioController.crearCuenta = async (req, res,next) =>{
     const {email, password} = req.body
     try {
         await Usuario.create({email, password})
+        const confirmarUrl = `http://${req.headers.host}/confirmar/${email}`
+        const usuario = {
+            email
+        }
+
+        await enviarEmail.enviar({
+        usuario,
+        email,
+        subject: 'Confirmar tu Cuenta UpTask!',
+        confirmarUrl,
+        archivo : 'confirmar-cuenta'
+        })
+
         res.redirect('/iniciar-sesion')
     } catch (error) {
         res.render('crearCuenta',{
@@ -22,6 +36,32 @@ usuarioController.formIniciarSesion =  (req, res,next) =>{
     res.render('iniciarSesion', {
         PageName: 'Iniciar Sesion'
     });
+}
+usuarioController.formReestablecerPassword =  (req, res,next) =>{
+    res.render('reestablecer', {
+        PageName: 'Reestablecer Contraseña'
+    });
+}
+usuarioController.confirmarCuenta = async (req, res) =>{
+    const email = req.params.email
+    const usuario = await Usuario.findOne({where: {email}})
+    if(!usuario) {
+        res.render('iniciarSesion', {
+            PageName: 'Iniciar Sesion',
+            error: 'No valido'
+        });
+    }
+    if(usuario.activo == 0){
+        usuario.activo = 1
+        usuario.save()
+        res.render('iniciarSesion', {
+            PageName: 'Iniciar Sesion',
+            mensaje: 'Cuenta Activada Correctamente!'
+        });
+    }else {
+        res.redirect('/iniciar-sesion')
+    }
+    
 }
 
 
